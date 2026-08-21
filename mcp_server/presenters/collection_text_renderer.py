d53b46dba92ab8dc38c6a4275588c4b802b5f27e
MikeyVK <michel@1voudig.com>
2026-08-21 23:59:48 +0200

    test(P_IMPLEMENTATION_SP_C3_RED): Define generic ordered-sequence presentation behavior for #456 (#456)

---
+++ mcp_server/presenters/collection_text_renderer.py
@@ -0,0 +1,6 @@
+# mcp_server/presenters/collection_text_renderer.py
+# template=service version=5d5b489a created=2026-08-21T21:57Z updated=2026-08-21
+"""Generic ordered-sequence presentation seam.
+
+@layer: Presenters
+"""
